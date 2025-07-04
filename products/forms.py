00b7d28fe58456a2bec4d08149677990b8fe1c35
Alexey <C:\Users\\AppData\Roaming\The Bat!>
2025-07-05 00:01:48 +0300

    Сделал печать ценников_исправил поиск

--- products/forms.py
+++ products/forms.py
@@ -1,37 +1,34 @@
-# products/forms.py (ФИНАЛЬНОЕ ВОССТАНОВЛЕНИЕ ПОИСКА И РЕГИСТРОНЕЗАВИСИМОСТЬ)
+# products/forms.py (ФИНАЛЬНОЕ ИСПРАВЛЕНИЕ СОРТИРОВКИ ПО НАЛИЧИЮ)
 
 from dal import autocomplete
 from .models import Product
 from .search_utils import get_product_search_queryset, format_product_for_display
-from django.db.models import QuerySet
-from django.db.models import Q as DjangoQ # Используем alias, чтобы не конфликтовать с Q в search_utils
+from django.db.models import QuerySet, Case, When # <--- ДОБАВЛЕНО: Case, When
 
 class ProductAutocomplete(autocomplete.Select2QuerySetView):
     """
     Автодополнение с надежной сортировкой в Python и регистронезависимым поиском.
     """
-    model = Product # Обязательно указываем модель для DAL
+    model = Product 
 
     def get_queryset(self):
-        # Получаем базовый QuerySet от родителя.
-        # super().get_queryset() может применять свою фильтрацию.
-        # Мы будем использовать свой, чтобы гарантировать регистронезависимость.
-        # Поэтому НЕ вызываем super().get_queryset() здесь,
-        # а начинаем с Product.objects.all()
         qs = Product.objects.all()
 
         if self.q:
-            # Наш get_product_search_queryset уже использует iregex и .lower()
             candidate_products = get_product_search_queryset(qs, self.q, fields=['name', 'sku'])
             
             words = self.q.lower().split()
 
-            def calculate_score(product):
+            # МЫ БУДЕМ СОРТИРОВАТЬ ТУТ, А ПОТОМ СОХРАНЯТЬ ПОРЯДОК
+            products_with_scores = []
+            for product in list(candidate_products): # <--- Обязательно конвертируем в список
                 score = 0
                 name_lower = product.name.lower()
                 
                 if product.get_available_stock_quantity > 0:
                     score += 10000 
+                else:
+                    score -= 10000 # Штраф за отсутствие в наличии
                 
                 if name_lower.startswith(self.q.lower()):
                     score += 1000
@@ -45,17 +42,27 @@ class ProductAutocomplete(autocomplete.Select2QuerySetView):
                     if word in name_lower:
                         score += 1
                 
-                return score
+                products_with_scores.append((score, product)) # Сохраняем счет и продукт
 
-            # Конвертируем QuerySet в список для сортировки в Python.
-            results_list = list(candidate_products)
-            results_list.sort(key=calculate_score, reverse=True)
+            # Сортируем список по счету (от большего к меньшему)
+            products_with_scores.sort(key=lambda x: x[0], reverse=True)
             
-            # Возвращаем QuerySet из отсортированных PK.
-            # Это должно сохранить порядок и быть регистронезависимым.
-            return Product.objects.filter(pk__in=[p.pk for p in results_list])
+            # Получаем отсортированные PK в правильном порядке
+            sorted_pks = [product.pk for score, product in products_with_scores]
+            
+            # Создаем QuerySet, который сохраняет этот порядок.
+            # Это делается с помощью Case и When.
+            # Если sorted_pks пуст, возвращаем пустой QuerySet.
+            if not sorted_pks:
+                return Product.objects.none()
+
+            # Создаем список When условий для каждого PK
+            preserved_order = Case(*[When(pk=pk, then=pos) for pos, pk in enumerate(sorted_pks)])
+            
+            # Фильтруем по PK и сортируем по сохраненному порядку
+            return Product.objects.filter(pk__in=sorted_pks).order_by(preserved_order)
         
-        # Если нет поискового запроса, возвращаем QuerySet с базовой сортировкой
+        # Если нет поискового запроса, возвращаем QuerySet с базовой сортировкой (как раньше)
         return qs.order_by('name')[:50]
         
     def get_result_value(self, result):
